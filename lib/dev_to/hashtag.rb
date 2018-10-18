f613d6d96a795e63743de24b36557f651fb18c53
Heather Miller <heather.miller114@gmail.com>
2018-10-18 19:14:25 +0000

    updated web loop

--- lib/dev_to/hashtag.rb
+++ lib/dev_to/hashtag.rb
@@ -11,52 +11,14 @@ class DevTo::Hashtag
     page = Nokogiri::HTML(open("https://dev.to/tags"))
     counter = 1
     while counter <= 20
-      page.css("a.tag-show-link h2").collect {|name| name.text}
-      #binding.pry
-      counter += 1
+      page.css("a.tag-show-link").css("h2").collect do |name| 
+        @@all_tags << name.text
+        counter += 1
+      end
     end
-
-    
-   
-    #puts "Here are the top 20 hashtags on DevTo"
-    #puts "1. Javascript"
-    #puts "2. Discuss"
-    #puts "3. WebDev"
-    #puts "4. Beginners"
-    #puts "5. React"
-    #puts "6. Career"
-    #puts "7. OpenSource"
-    #puts "8. Productivity"
-    #puts "9. Node"
-    #puts "10. CSS"
-    #puts "11. Python"
-    #puts "12. Help"
-    #puts "13. DevOps"
-    #puts "14. ShowDev"
-    #puts "15. Testing"
-    #puts "16. Java"
-    #puts "17. Programming"
-    #puts "18. Git"
-    #puts "19. PHP"
-    #puts "20. Ruby"
-    
-    
-    #hashtag_1 = self.new
-    #hashtag_1.name = "Javascript"
-    #hashtag_1.url = "https://dev.to/t/javascript"
-    #@@all_tags << hashtag_1
-
-    #hashtag_2 = self.new
-    #hashtag_2.name = "Discuss"
-    #hashtag_2.url = "https://dev.to/t/discuss"
-    #@@all_tags << hashtag_2
-    
-    #hashtag_3 = self.new
-    #hashtag_3.name = "WebDev"
-    #hashtag_3.url = "https://dev.to/t/webdev"
-    #@@all_tags << hashtag_3
+    binding.pry
+    @@all_tags
 
   end
-  
   
 end
